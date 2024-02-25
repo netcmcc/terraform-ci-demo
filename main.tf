7d47a8075a4a291d6033e6a5844f5c808a8eadb6
Ming Chen <netcmcc@outlook.com>
2024-02-26 01:44:47 +0800

    111

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ resource "tencentcloud_vpc" "vpc" {
   cidr_block = var.vpc_cidr
 }
 
-resource "tencentcloud_subnet" "subnet" {
-  for_each = { for subnet in var.subnets : subnet.name => subnet }
+# resource "tencentcloud_subnet" "subnet" {
+#   for_each = { for subnet in var.subnets : subnet.name => subnet }
 
-  vpc_id            = tencentcloud_vpc.vpc.id
-  name              = each.value.name
-  cidr_block        = each.value.cidr
-  availability_zone = each.value.zone
-}
+#   vpc_id            = tencentcloud_vpc.vpc.id
+#   name              = each.value.name
+#   cidr_block        = each.value.cidr
+#   availability_zone = each.value.zone
+# }
